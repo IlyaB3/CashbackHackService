ffe26fcd012bbd794ebcf023c07f89e0e2e7a4b8
IlyaB <ilyaberd3@gmail.com>
2023-04-23 18:00:50 +0300

    firstTestNg

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,52 @@
 package ru.netology.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
 
-class CashbackHackServiceTest {
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    void testShowingTheNeedToCubForLargeAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(actual,expected);
+    }
+    @Test
+    void testWithAPurchaseAmountOf0Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual,expected);
+    }
+    @Test
+    void testWithAPurchaseAmountOfMoreThan1000Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1011;
+
+        int actual = service.remain(amount);
+        int expected = 989;
+
+        assertEquals(actual,expected);
+    }
+    @Test
+    void testWithAPurchaseAmountOf1000Rubles() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual,expected);
+    }
+
+
+
 
 }
